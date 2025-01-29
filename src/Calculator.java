c15a8b13c0a25538ee985fb28b2d803f3998e130
PowerlineR413 <Powerline413@yandex.ru>
2025-01-29 16:45:58 +0300

    Мой первый калькулятор 1.0

---
+++ src/Calculator.java
@@ -0,0 +1,60 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author El Baron 22.07.2024
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        double firstNum;
+        do {
+            System.out.println("Введите первое число:");
+            try {
+                firstNum = new Scanner(System.in).nextDouble();
+            } catch (InputMismatchException exception) {
+                System.out.println("Введённо неподходящее значение");
+                continue;
+            }
+            break;
+        } while (true);
+
+        System.out.println("Введите необходимый оператор(+ - / *)");
+        String operator = new Scanner(System.in).nextLine();
+
+        double secondNum;
+        do {
+            System.out.println("Введите второе число:");
+            try {
+                secondNum = new Scanner(System.in).nextDouble();
+            } catch (InputMismatchException exception) {
+                System.out.println("Введено неподходящее значение");
+                continue;
+            }
+            break;
+        } while (true);
+
+        double answer;
+
+
+        switch (operator) {
+            case "+":
+                answer = firstNum + secondNum;
+                System.out.println(firstNum + " + " + secondNum + " = " + answer);
+                break;
+            case "-":
+                answer = firstNum - secondNum;
+                System.out.println(firstNum + " - " + secondNum + " = " + answer);
+                break;
+            case "/":
+                answer = firstNum / secondNum;
+                System.out.println(firstNum + " / " + secondNum + " = " + answer);
+                break;
+            case "*":
+                answer = firstNum * secondNum;
+                System.out.println(firstNum + " * " + secondNum + " = " + answer);
+                break;
+
+        }
+    }
+}
+
